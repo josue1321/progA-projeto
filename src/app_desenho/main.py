97e27951c182575634c853e0bc34366b414446c3
Gustavo Reis <gustavoreisg7462@gmail.com>
2026-07-13 19:38:42 -0300

    Importação das funções da biblioteca tkinter para criação da interface

---
+++ src/app_desenho/main.py
@@ -0,0 +1,4 @@
+# Importa as ferramentas gráficas do Tkinter 
+from tkinter import Tk, Canvas, Frame, StringVar, ttk, W
+# Importa as classes de desenho do arquivo figuras.py
+from figuras import Linha, Rabisco, Retangulo, Oval, Circulo, Poligono
